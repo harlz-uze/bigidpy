9d2e9391235c6bd841d1788bb17574fb48b11db9
Administrator <bharley@bigid.com>
2023-07-24 14:54:19 +1000

    added get_classifiers function

--- classifier_engine.py
+++ classifier_engine.py
@@ -4,12 +4,28 @@ __version__ = "1.0.0"
 __status__ = "Development"
 
 import bigid
-from data_types import BigData, Classifier
+from data_types import BigData, RegexClassifier, DocClassifier, NerClassifier
 
-def get_classifiers(bigid: bigid.BigID) -> list[Classifier]:
+def get_classifiers(bigid: bigid.BigID) -> dict[str, list[RegexClassifier, NerClassifier, DocClassifier]]:
     ''' Get classifiers from BigID and return them as a list'''
     data = bigid.make_request(api_path='/api/v1/classifiers', http_method='GET')
-    res: list[Classifier] = []
+    res: dict[str, list[RegexClassifier, DocClassifier, NerClassifier]] = {'RegexClassifiers': [], 'NerClassifiers': [], 'DocClassifiers': [], 'failures': []}
     for i in data.data['data']['classifiers']:
-        res.append(Classifier.from_dict(i))
+        if 'classification_regex' in i['originalData'][0]:
+            # print(i['originalData'][0])
+            res['RegexClassifiers'].append(RegexClassifier.from_dict(i['originalData'][0]))
+        elif 'type' in i['originalData'][0] and i['originalData'][0]['type'] == 'DOC':
+            res['DocClassifiers'].append(DocClassifier.from_dict(i['originalData'][0]))
+        elif 'type' in i['originalData'][0] and i['originalData'][0]['type'] == 'NER':
+            res['NerClassifiers'].append(NerClassifier.from_dict(i['originalData'][0]))
+        else:
+            res['failures'].append(i['originalData'][0])
+            # raise ClassifierError(message='Unsupported Classifier type. Classifier is not Regex, NER Or DOC')
+    if len(res['failures']) > 0:
+        print('Failed to parse classifier, supported types are DOC, NER & Regex see result["failures"] for more info')
     return res
+
+def add_classifier(bigid: bigid.BigID, classifier: RegexClassifier) -> None:
+    ''' Add a classifier to the BigID instance '''
+    data = bigid.make_request(api_path='/api/v1/classifications', http_method='GET')
+    pass
--- data_types.py
+++ data_types.py
@@ -3,7 +3,7 @@ __author__ = "Brenton Harley"
 __version__ = "1.0.0"
 __status__ = "Development"
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
 from typing import Optional
 
@@ -56,15 +56,67 @@ class BigIdPolicy:
     
 @dataclass_json
 @dataclass
-class Classifier:
-    id: str
+class RegexClassifier:
+    # id: str
+    # name: str
+    # attributeRiskName: str
+    # type: enumerate
+    # totalFindings: int
+    # originalData: list[dict[str, str]]
+    # nerTitle: Optional[str] =None
+    # Allowed: Optional[str] =None #doc┃ner┃meta┃data
+    _id: str
+    classification_name: str
+    classification_regex: str
     name: str
-    attributeRiskName: str
-    type: enumerate
-    totalFindings: int
-    originalData: list[dict[str, str]]
-    nerTitle: Optional[str] =None
-    Allowed: Optional[str] =None #doc┃ner┃meta┃data
+    proximity_before: Optional[int] = None
+    version: Optional[str] = None
+    updated_at: Optional[str] = None
+    support_term_value: Optional[str] = None
+    enabled: Optional[bool] = None
+    proximity_after: Optional[int] = None
+    validation: Optional[str] = None
+    max_length: Optional[int] = None
+    min_length: Optional[int] = None
+
+    # detection_sensitivity: Optional[int] = None
+    # description: Optional[str] = None
+    # hierachy: Optional[list[str]] = field(default_factory=list)
+    # category: Optional[list[str]] = field(default_factory=list)
+    # country_code: Optional[list[str]] = field(default_factory=list)
+    # classification_regex: Optional[str] = None
+    # nerTitle: Optional[str] = None
+    # Allowed: Optional[str] = None
+    # classifierType: Optional[str] = None
+
+    # isOutOfTheBox: Optional[bool] = None
+    # performance: Optional[int] = None
+
+@dataclass_json
+@dataclass
+class NerClassifier:
+    _id: str
+    type: str
+    updated_at: str
+    name: str
+    version: Optional[str] = None
+    classifierName: Optional[str] = None
+    
+@dataclass_json
+@dataclass
+class DocClassifier:
+    _id: str
+    category_name: list[str]
+    defaultWeightRisk: int
+    detection_sensitivity: int
+    enabled: bool
+    modelId: str
+    nerEntityName: str
+    type: str
+    updated_at: str
+    name: str
+    country_Code: Optional[list[str]] = None
+    
     # _id
     # string
     # classification_name
